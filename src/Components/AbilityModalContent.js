ddac082065078717ebe6665ada8460047a268fc0
jlhedric <jadehedrick1@gmail.com>
2023-09-16 15:01:41 -0400

    where we're going, we dont need state

--- src/Components/AbilityModalContent.js
+++ src/Components/AbilityModalContent.js
@@ -4,84 +4,9 @@ import React from 'react'
 
 
 const AbilityModalContent = ({ onClose, partyAttributes, caster, name, targetType, onSubmit}) => {
-  let initialVal = new Set();
-  if(['self', 'partner'].includes(targetType)) {
-    initialVal.add(caster)
-  }
-  const [targetList, setTargetList] = React.useState(initialVal)
   const players = Object.keys(partyAttributes)
 
-  const handleChange = (e) => {
-    const target = e.target.name
-    setTargetList((prevState) => {
-      let newTargets = prevState
-      // newTargets.add(target) ? e.target.checked : newTargets.delete(target) DOESNT WORK FOR SINGLE
-      return newTargets
-    })
-  };
-
   const TargetInput = () => {
-    // TODO: figure out target counts? also how to set caster as immutable option
-    if(targetType === 'partner') {
-      return (
-        players.map((player) => {
-          if (player === caster) {
-            return (
-              <label key={player}>{partyAttributes[player]['job']} 
-                <input 
-                  type='checkbox' 
-                  name={player} 
-                  defaultChecked={true}
-                  disabled
-                />
-              </label>
-            )
-          }
-          else {
-            return (
-              <label key={player}>{partyAttributes[player]['job']} 
-                <input 
-                  type='radio' 
-                  name={player} 
-                  defaultChecked={false}
-                  onChange={handleChange}
-                />
-              </label>
-            )
-          }
-        })
-      )
-    }
-    if(targetType === 'anyButSelf') {
-      return (
-        players.map((player) => {
-          if (player === caster) {
-            return (
-              <label key={player}>{partyAttributes[player]['job']} 
-                <input 
-                  type='radio' 
-                  name={player} 
-                  defaultChecked={false}
-                  disabled
-                />
-              </label>
-            )
-          }
-          else {
-            return (
-              <label key={player}>{partyAttributes[player]['job']} 
-                <input 
-                  type='radio' 
-                  name={player} 
-                  defaultChecked={false}
-                  onChange={handleChange}
-                />
-              </label>
-            )
-          }
-        })
-      )
-    }
     if(targetType === 'single') {
       return (
         players.map((player) => (
@@ -89,16 +14,43 @@ const AbilityModalContent = ({ onClose, partyAttributes, caster, name, targetTyp
             <input 
               type='radio' 
               name='targets'
-              defaultChecked={false}
-              onChange={handleChange}/>
+              defaultChecked={false} />
           </label>
         ))
       )
     }
-
+    else {
+      return (
+        players.map((player) => {
+          if (player === caster) {
+            return (
+              <label key={player}>{partyAttributes[player]['job']} 
+                <input 
+                  type='radio' 
+                  name='self' 
+                  defaultChecked={(targetType === 'partner')}
+                  value={player}
+                  disabled
+                />
+              </label>
+            )
+          }
+          else {
+            return (
+              <label key={player}>{partyAttributes[player]['job']} 
+                <input 
+                  type='radio' 
+                  name='targets'  
+                  defaultChecked={false}
+                  value={player}
+                />
+              </label>
+            )
+          }
+        })
+      )
+    }
   }
-
-
   return (
       <div className='modal'>
         <form onSubmit={onSubmit}>
